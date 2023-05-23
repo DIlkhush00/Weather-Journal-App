bd2882c55e307c8b56833bf85a504fdab678de04
Dilkhush <dilkhushpurohit01@gmail.com>
2023-05-23 22:23:46 +0530

    update app.js

--- website/app.js
+++ website/app.js
@@ -1,5 +1,5 @@
 // Personal API Key for OpenWeatherMap API
-const APIKey = '8a1b1ab0fe04dcf234b939474ad5d92a';
+const APIKey = '<your-api-key>';
 const baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
 const appData = {};
 
